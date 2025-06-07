ea9bb8749fbeeac7d119b693371aa5b86ed06d48
jerinmulangan <ejerinmulangan@gmail.com>
2025-06-07 04:43:51 -0500

    restructured repo

--- compiler/TL13/output_badif.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
-int X = 0;
-X = 5;
--- compiler/TL13/tl13_codegen.h
+++
@@ -1,7 +0,0 @@
-#ifndef TL13_CODEGEN_H
-#define TL13_CODEGEN_H
-
-#include "tl13_ast.h"
-void generate_code(ASTNode *node);
-
-#endif
